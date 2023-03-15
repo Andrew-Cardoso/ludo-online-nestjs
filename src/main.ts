d7a4fa66d7a131a05417cc18e90b4e6e3f170797
Andrew Cardoso <andrewgcardoso@gmail.com>
2023-03-15 05:13:01 -0300

    added logs to main

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ dotenv.config();
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 (async () => {
   const app = await NestFactory.create(AppModule);
   await app.init();
+  new Logger('Application').warn(
+    `Application is running on ${process.env.PORT}`,
+  );
 })();
