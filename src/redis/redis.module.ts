139259b24a3d8c01bd04b5b73849c32d1d08b12f
Andrew Cardoso <andrewgcardoso@gmail.com>
2023-03-15 12:22:52 -0300

    feat: listening to redis events

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,4 +1,4 @@
-import { Global, Module } from '@nestjs/common';
+import { Global, Logger, Module } from '@nestjs/common';
 import { createClient } from 'redis';
 
 @Global()
@@ -7,8 +7,16 @@ import { createClient } from 'redis';
     {
       provide: 'REDIS',
       useFactory: async () => {
+        const logger = new Logger('Redis');
         const client = createClient({ url: process.env.REDIS_URL });
+
+        client.on('error', (err) => logger.error(err));
+        client.on('connect', () => logger.log('Connected to Redis'));
+        client.on('reconnecting', () => logger.warn('Redis is reconnecting'));
+        client.on('ready', () => logger.log('Redis is ready'));
+
         await client.connect();
+
         return client;
       },
     },
